dd9b465e5ffc38917cc7866b9e290dc63e913058
emmaiwasaki <emmaiwasaki@berkeley.edu>
2023-12-06 17:19:51 -0800

    added howBig

---
+++ JS101/howBig.js
@@ -0,0 +1,12 @@
+const input = require('readline-sync');
+
+console.log('enter the length of the room in meters: ')
+let length = input.prompt();
+
+console.log('enter the width of the room in meters: ')
+let width = input.prompt();
+let areaMeters = (length * width)
+let areaSquareFeet = areaMeters * 10.7639
+
+
+console.log(`the area of the room is ${areaMeters.toFixed()} square meters (${areaSquareFeet.toFixed()} square feet)`);
