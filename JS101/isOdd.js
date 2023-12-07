095cec3ce361743eb888ad93a7eaaf3ecb8494e2
emmaiwasaki <emmaiwasaki@berkeley.edu>
2023-12-06 16:26:15 -0800

    added isOdd

--- JS101/isOdd.js
+++ JS101/isOdd.js
@@ -1,1 +1,14 @@
-// comment
+const isOdd = (int) => {
+    if (int % 2 === 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(isOdd(2)); // => false
+console.log(isOdd(5)); // => true
+console.log(isOdd(-17)); // => true
+console.log(isOdd(-8)); // => false
+console.log(isOdd(0)); // => false
+console.log(isOdd(7)); // => true
