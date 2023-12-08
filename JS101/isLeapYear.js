1f1ea408e334146b635a1b125b8722ee2dfb31a0
emmaiwasaki <emmaiwasaki@berkeley.edu>
2023-12-07 18:52:33 -0800

    added isLeapYear part 2

--- JS101/isLeapYear.js
+++ JS101/isLeapYear.js
@@ -1,6 +1,11 @@
 const isLeapYear = (num) => {
-    console.log((num % 4 === 0 && num % 100 !== 0) || (num % 400 === 0));
+    if (num > 1752) {
+        console.log((num % 4 === 0 && num % 100 !== 0) || (num % 400 === 0));
+    } else {
+        console.log(num % 4 === 0);
+    }
 }
+
 isLeapYear(2016);      // true
 isLeapYear(2015);      // false
 isLeapYear(2100);      // false
@@ -10,8 +15,8 @@ isLeapYear(240001);    // false
 isLeapYear(2000);      // true
 isLeapYear(1900);      // false
 isLeapYear(1752);      // true
-isLeapYear(1700);      // false
+isLeapYear(1700);      // true
 isLeapYear(1);         // false
-isLeapYear(100);       // false
+isLeapYear(100);       // true
 isLeapYear(400);       // true
 
